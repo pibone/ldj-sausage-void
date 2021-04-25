3d803537b32d2fe982e0b83fff2bfbcf4046cf2d
jypuigbo <jordiysard@gmail.com>
2021-04-25 16:05:43 +0100

    Recovered functionality from JY's version

--- Assets/Scripts/SpamFood.cs
+++ Assets/Scripts/SpamFood.cs
@@ -11,8 +11,10 @@ public class SpamFood : MonoBehaviour
     {
         var x = Random.Range(min_X,max_X);
         var y = Random.Range(min_Y,max_Y);
-        GameObject newObject = Instantiate(sampleFood, new Vector2(x, y), Quaternion.Euler(0, 0, Random.Range(0f,360f)));
-        newObject.transform.localScale = new Vector3(size,1,1);
+        GameObject newObject = Instantiate(sampleFood, new Vector3(x, y,-1), Quaternion.Euler(0, 0, Random.Range(0f,360f)));
+        newObject.transform.localScale = new Vector3(size*10,10,1);
+        newObject.transform.SetParent (GameObject.FindGameObjectWithTag("Canvas").transform, false);
+        
     }
 
     // Start is called before the first frame update
