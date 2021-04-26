6a0430b7022919d1c616fbad1c883a77513eb955
jypuigbo <jordiysard@gmail.com>
2021-04-26 13:38:06 +0100

    Added timer and GameOver

--- Assets/Scripts/sausageCounter.cs
+++ Assets/Scripts/sausageCounter.cs
@@ -5,18 +5,27 @@ using UnityEngine.UI;
 
 public class sausageCounter : MonoBehaviour
 {
-        public Text textBox;
+        public Text score, timeText;
         private GameController gameControllerObject;
+        private LevelController levelControllerObject;
+        private float timer=0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         gameControllerObject = GameObject.FindWithTag("gameControllerObject").GetComponent<GameController>();
+        levelControllerObject = GameObject.FindWithTag("levelControllerObject").GetComponent<LevelController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        textBox.text=gameControllerObject.GetSausages().ToString();
+        timer += Time.deltaTime;
+         int seconds = (int) (timer % 60);
+         timeText.text = seconds.ToString();
+        score.text=gameControllerObject.GetSausages().ToString();
+        if (seconds >= 30){
+           levelControllerObject.LoadEnding();
+        }
     }
 }
